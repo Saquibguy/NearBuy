978ca4c50b65fef77f1b04151f7a80e5cc3ff8dd
Twilight <chaudharysaquib8@gmail.com>
2026-09-16 15:50:28 +0530

    Allow admin role in user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -29,7 +29,7 @@ const userSchema = new mongoose.Schema(
 
     role: {
       type: String,
-      enum: ['customer', 'seller'],
+      enum: ['customer', 'seller', 'admin'],
       required: true,
     },
 
